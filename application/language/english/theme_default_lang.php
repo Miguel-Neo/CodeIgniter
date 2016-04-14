59ddc0f9386a5c149849fce800842fabc314b8fb
miguel <miguel.neommunications@gmail.com>
2016-04-14 18:42:10 -0500

    diccionarios de prueba

---
+++ application/language/english/theme_default_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['theme_default_lang_test'] = 'Error loading theme';
+$lang['theme_default_lang_test2'] = 'Error loading view';
---
+++ application/language/spanish/theme_default_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['theme_default_lang_test'] = 'Error espera theme';
+$lang['theme_default_lang_test2'] = 'Error espera view';
