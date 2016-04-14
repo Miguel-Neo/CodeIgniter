48de9f1267896b9d9a23ce850183014619028926
miguel <miguel.neommunications@gmail.com>
2016-04-14 18:43:27 -0500

    comentario
    se define que la configuración de estos idiomas afecta para eso dos
    archivos

--- application/config/cms.php
+++ application/config/cms.php
@@ -2,6 +2,7 @@
 
 $config['cms_admin_panel_uri'] = 'admin';
 $config['cms_languages'] = ['english','spanish'];
+# estos idiomas son para los diccionarios {cms_general_lang.php} y {theme_default_lang.php}
 
 
 /* End of file cms.php */
